ce1ad6a57e83d5f22dd595b1829809ed36dcd632
Shrey Bana <shreybana26@gmail.com>
2025-04-11 21:32:42 +0530

    Wrapped gradle to set jdk home.

--- flake.nix
+++ flake.nix
@@ -16,7 +16,10 @@
       {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
-            gradle_8
+            ## Gradle is a wrapped bin & resets the JAVA_HOME env.
+            ## So to workaround it I've used a gradle property to point to the
+            ## nix store jdk.
+            (writeShellScriptBin "gradle" "${pkgs.gradle_8}/bin/gradle -Porg.gradle.java.installations.paths=${pkgs.jdk17}/lib/openjdk \${@}")
             jdk17
           ];
         };
