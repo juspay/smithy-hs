6550e9140fb12d76957358aebc3e31fcc64661c3
Shrey Bana <shrey.bana@juspay.in>
2025-05-07 13:39:02 +0530

    Parenthesized nested symbols.

--- client-codegen/src/main/kotlin/io/superposition/smithy/haskell/client/codegen/SymbolExt.kt
+++ client-codegen/src/main/kotlin/io/superposition/smithy/haskell/client/codegen/SymbolExt.kt
@@ -6,3 +6,12 @@ import kotlin.jvm.optionals.getOrDefault
 fun Symbol.isPrimitive(): Boolean = this.getProperty(SymbolProperties.IS_PRIMITIVE).getOrDefault(false)
 
 fun Symbol.wrap(sym: Symbol) = sym.toBuilder().addReference(this).build()
+
+fun Symbol.toMaybe() = this.wrap(HaskellSymbol.Maybe)
+
+fun Symbol.toEither(right: Symbol) = right.wrap(HaskellSymbol.Either)
+    .toBuilder()
+    .addReference(this)
+    .build()
+
+fun Symbol.inIO() = this.wrap(HaskellSymbol.IO)
