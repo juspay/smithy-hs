40e4a072ea5d2d44b2f426a96b9873f45bfc0a17
Shrey Bana <shrey.bana@juspay.in>
2025-05-21 13:40:20 +0530

    build: Replaced detekt w/ ktlint.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,9 @@
-// In your root build.gradle.kts file
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.detekt)
+    alias(libs.plugins.ktlint)
 }
-
-val detektV = libs.versions.detekt.get()
-val detektFormatter = libs.detekt.formatting
 
 allprojects {
     repositories {
@@ -13,48 +11,22 @@ allprojects {
         google()
         gradlePluginPortal()
     }
+}
 
-    apply(plugin = "io.gitlab.arturbosch.detekt")
-
-    detekt {
-        toolVersion = detektV // Keep in sync with plugin version
-        config.setFrom(files("$rootDir/detekt.yml"))
-        buildUponDefaultConfig = true // Adds default rules to your custom config
-        autoCorrect = true // Automatically fixes issues when possible
-        parallel = true // Improves performance on multicore machines
+ktlint {
+    verbose.set(true)
+    outputToConsole.set(true)
+    coloredOutput.set(true)
+    enableExperimentalRules.set(true)
+    reporters {
+        reporter(ReporterType.CHECKSTYLE)
+        reporter(ReporterType.PLAIN)
     }
-
-    dependencies {
-        detektPlugins(detektFormatter)
-    }
-
-    // Configure reports on the task level instead
-    tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
-        config.setFrom(files("$rootDir/detekt.yml"))
-        reports {
-            html.required.set(true)
-            xml.required.set(false)
-            txt.required.set(false)
-            sarif.required.set(true)
-        }
+    filter {
+        exclude { element -> element.file.path.contains("generated/") }
     }
 }
 
-// Configure detekt to run as part of the check task
-tasks.named("check") { dependsOn(tasks.named("detekt")) }
-
-// Create a specific task just for fixing issues
-tasks.register<io.gitlab.arturbosch.detekt.Detekt>("detektFix") {
-    description = "Fix Detekt issues."
-    setSource(files("src/main/kotlin", "src/test/kotlin"))
-    config.setFrom(files("$rootDir/detekt.yml"))
-    buildUponDefaultConfig = true
-    parallel = true
-    autoCorrect = true // Ensure auto-correct is enabled
-    disableDefaultRuleSets = false
-    debug = true
-
-    reports {
-        html.required.set(true)
-    }
+tasks.named("check") {
+    dependsOn("ktlintCheck")
 }
